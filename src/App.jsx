71df0be45cf5af4ec75a7ec399cb6a95c3888556
Agustín Rodríguez <arberoois@gmail.com>
2025-11-14 23:30:16 -0300

    website under construction

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="construction-container">
+      <div className="construction-card">
+        <h1 className="construction-title">
+          Bienvenidos a <span className="brand">AppetiteUY</span>
+        </h1>
+
+        <p className="construction-subtitle">Nuestro sitio web está en construcción.</p>
+        <p className="construction-text">
+          Pronto podrás descubrir más sobre nuestra plataforma.
+        </p>
+      </div>
+    </div>
+  );
+}
